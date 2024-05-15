dad9ba35ee8dbd41c52aefe308e158c4a58478f0
Muhammadg'ozi <goziyevmuhammadgozi@gmail.com>
2024-05-15 13:48:10 +0500

    update login page task completed

--- src/components/Loader/index.jsx
+++ src/components/Loader/index.jsx
@@ -3,12 +3,7 @@ import { FadeLoader } from "react-spinners";
 function Loader() {
   return (
     <div className="absolute inset-0 z-[1] flex items-center justify-center bg-gray-900 bg-opacity-55 backdrop-blur-sm  ">
-      <FadeLoader
-        color="#4283cc"
-        speedMultiplier={1.1}
-        height={40}
-        margin={20}
-      />
+      <FadeLoader color="#ff0000" height={50} margin={30} />
     </div>
   );
 }
