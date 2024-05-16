6c2531d147510205e2e9cc085d2f5ee3e364f4a6
ibrohimov-murodjon <murodjon.ibrohimov.rishton@gmail.com>
2024-05-16 18:32:42 +0500

    home page ended

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Route, Routes, useNavigate } from "react-router-dom";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import RoutesLayout from "./Layout/RoutesLayout.jsx";
 import { jwtDecode } from "jwt-decode";
 import {
@@ -14,33 +14,32 @@ import {
 } from "./Pages";
 import { useDispatch, useSelector } from "react-redux";
 import { setRole } from "./store/userToken";
+import GlobalStyles from "./styles/GlobalStyles";
 
 function App() {
   const token = useSelector((state) => state.userToken.token);
   const role = useSelector((state) => state.userToken.role);
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
   const navigate = useNavigate();
-  
 
   function fetchUserRole() {
-  if (token) {
-    const decodedToken = jwtDecode(token);
-    const userRole = decodedToken.user_roles;
-    const currentTime = Date.now() / 1000;
-    dispatch(setRole(userRole));
-    if (decodedToken.exp < currentTime) {
-      navigate("/login");
+    if (token) {
+      const decodedToken = jwtDecode(token);
+      const userRole = decodedToken.user_roles;
+      const currentTime = Date.now() / 1000;
+      dispatch(setRole(userRole));
+      if (decodedToken.exp < currentTime) {
+        navigate("/login");
+      }
     }
   }
-}
-
 
   useEffect(() => {
     if (!localStorage.getItem("accessToken")) {
       navigate("/login");
     }
-    fetchUserRole()
-  }, [navigate,token]);
+    fetchUserRole();
+  }, [navigate, token]);
 
   function ProtectedRoute({
     children,
@@ -57,92 +56,95 @@ function App() {
   }
 
   return (
-    <Routes>
-      <Route path="/login" element={<Login />} />
-      <Route path="*" element={<ErrorPage />} />
-      {token && role === "admin" && (
-        <>
-          <Route
-            path="/"
-            element={
-              <ProtectedRoute
-                isAuthentication={token ? true : false}
-                redirectTo="/login"
-              >
-                <RoutesLayout>
-                  <Home />
-                </RoutesLayout>
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/product"
-            element={
-              <ProtectedRoute
-                isAuthentication={token ? true : false}
-                redirectTo="/login"
-              >
-                <RoutesLayout>
-                  <Product />
-                </RoutesLayout>
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/order"
-            element={
-              <ProtectedRoute
-                isAuthentication={token ? true : false}
-                redirectTo="/login"
-              >
-                <RoutesLayout>
-                  <Order />
-                </RoutesLayout>
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/message"
-            element={
-              <ProtectedRoute
-                isAuthentication={token ? true : false}
-                redirectTo="/login"
-              >
-                <RoutesLayout>
-                  <Message />
-                </RoutesLayout>
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/xodimlar"
-            element={
-              <ProtectedRoute
-                isAuthentication={token ? true : false}
-                redirectTo="/login"
-              >
-                <RoutesLayout>
-                  <Xodimlar />
-                </RoutesLayout>
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/profile"
-            element={
-              <ProtectedRoute
-                isAuthentication={token ? true : false}
-                redirectTo="/login"
-              >
-                <RoutesLayout>
-                  <Profile />
-                </RoutesLayout>
-              </ProtectedRoute>
-            }
-          />
-        </>
-      )}
-    </Routes>
+    <>
+      <GlobalStyles />
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="*" element={<ErrorPage />} />
+        {token && role === "admin" && (
+          <>
+            <Route
+              path="/"
+              element={
+                <ProtectedRoute
+                  isAuthentication={token ? true : false}
+                  redirectTo="/login"
+                >
+                  <RoutesLayout>
+                    <Home />
+                  </RoutesLayout>
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/product"
+              element={
+                <ProtectedRoute
+                  isAuthentication={token ? true : false}
+                  redirectTo="/login"
+                >
+                  <RoutesLayout>
+                    <Product />
+                  </RoutesLayout>
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/order"
+              element={
+                <ProtectedRoute
+                  isAuthentication={token ? true : false}
+                  redirectTo="/login"
+                >
+                  <RoutesLayout>
+                    <Order />
+                  </RoutesLayout>
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/message"
+              element={
+                <ProtectedRoute
+                  isAuthentication={token ? true : false}
+                  redirectTo="/login"
+                >
+                  <RoutesLayout>
+                    <Message />
+                  </RoutesLayout>
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/xodimlar"
+              element={
+                <ProtectedRoute
+                  isAuthentication={token ? true : false}
+                  redirectTo="/login"
+                >
+                  <RoutesLayout>
+                    <Xodimlar />
+                  </RoutesLayout>
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/profile"
+              element={
+                <ProtectedRoute
+                  isAuthentication={token ? true : false}
+                  redirectTo="/login"
+                >
+                  <RoutesLayout>
+                    <Profile />
+                  </RoutesLayout>
+                </ProtectedRoute>
+              }
+            />
+          </>
+        )}
+      </Routes>
+    </>
   );
 }
 
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,80 +1,97 @@
-import { Dialog } from "@material-tailwind/react";
-import OutlinedInput from "../components/OutlineInput";
-import { useState } from "react";
-import OutlineModal from "../components/OutlineModal/OutlineModal";
-import Button from "../components/Button/Button";
-import OutlineDeleteModal from "../components/OutlineDeleteModal/OutlineDeleteModal";
-
-function Home() {
-  const [openUserAdModal, setopenUserAdModal] = useState(false);
-  const [dataUpdate, setDataUpdate] = useState(false);
-  const [selectedTemplate, setSelectedTemplate] = useState({});
-  const [isSuccess, setIsSucces] = useState(false);
-  const [message, setMessage] = useState(false);
-  const [openSnackbar, setOpenSnackbar] = useState(false);
-
-  const [size, setSize] = useState(null);
-
-  const handleOpen = (value) => setSize(value);
-
-  const addUsersFun = () => {
-    setopenUserAdModal(true);
-  };
-
-  const deleteCloseFun = () => {
-    setSize(null);
-  };
-
-  const [eskizValues, setEskizValues] = useState({
-    email: "",
-    emailError: false,
-    secretKey: "",
-    secretKeyError: false,
-  });
-
-  const handleEskizChange = (e) => {
-    const { name, value } = e.target;
-    setEskizValues((prevState) => ({
-      ...prevState,
-      [name]: value,
-      [`${name}Error`]: false,
-    }));
-  };
-
+import DashboardFilter from "../components/dashboard/DashboardFilter";
+import DashboardLayout from "../components/dashboard/DashboardLayout";
+import Heading from "../components/Heading";
+const Home = () => {
   return (
-    <>
-      <Button width="130px" bgColor="black" value="Text" />
-      <button onClick={() => handleOpen("xs")}>Open</button>
-      <Dialog
-        open={
-          size === "xs" ||
-          size === "sm" ||
-          size === "md" ||
-          size === "lg" ||
-          size === "xl" ||
-          size === "xxl"
-        }
-        size={size || "md"}
-        handler={handleOpen}
-        onClose={() => deleteCloseFun()}
-        sx={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <OutlineDeleteModal
-          handleClose={deleteCloseFun}
-          selectedItem={selectedTemplate}
-          updateFun={dataUpdate}
-          setIsSucces={setIsSucces}
-          setMessage={setMessage}
-          setOpenSnackbar={setOpenSnackbar}
-        />
-      </Dialog>
-    </>
+    <div className="p-10">
+      <div className="flex justify-between items-center">
+        <Heading as="h2">Dashboard</Heading>
+        <DashboardFilter />
+      </div>
+      <DashboardLayout />
+    </div>
   );
-}
-Home.propTypes = {};
+};
 
 export default Home;
+
+// import { Dialog } from "@material-tailwind/react";
+// import OutlinedInput from "../components/OutlineInput";
+// import { useState } from "react";
+// import OutlineModal from "../components/OutlineModal/OutlineModal";
+// import Button from "../components/Button/Button";
+// import OutlineDeleteModal from "../components/OutlineDeleteModal/OutlineDeleteModal";
+
+// function Home() {
+//   const [openUserAdModal, setopenUserAdModal] = useState(false);
+//   const [dataUpdate, setDataUpdate] = useState(false);
+//   const [selectedTemplate, setSelectedTemplate] = useState({});
+//   const [isSuccess, setIsSucces] = useState(false);
+//   const [message, setMessage] = useState(false);
+//   const [openSnackbar, setOpenSnackbar] = useState(false);
+
+//   const [size, setSize] = useState(null);
+
+//   const handleOpen = (value) => setSize(value);
+
+//   const addUsersFun = () => {
+//     setopenUserAdModal(true);
+//   };
+
+//   const deleteCloseFun = () => {
+//     setSize(null);
+//   };
+
+//   const [eskizValues, setEskizValues] = useState({
+//     email: "",
+//     emailError: false,
+//     secretKey: "",
+//     secretKeyError: false,
+//   });
+
+//   const handleEskizChange = (e) => {
+//     const { name, value } = e.target;
+//     setEskizValues((prevState) => ({
+//       ...prevState,
+//       [name]: value,
+//       [`${name}Error`]: false,
+//     }));
+//   };
+
+//   return (
+//     <>
+//       <Button width="130px" bgColor="black" value="Text" />
+//       <button onClick={() => handleOpen("xs")}>Open</button>
+//       <Dialog
+//         open={
+//           size === "xs" ||
+//           size === "sm" ||
+//           size === "md" ||
+//           size === "lg" ||
+//           size === "xl" ||
+//           size === "xxl"
+//         }
+//         size={size || "md"}
+//         handler={handleOpen}
+//         onClose={() => deleteCloseFun()}
+//         sx={{
+//           display: "flex",
+//           justifyContent: "center",
+//           alignItems: "center",
+//         }}
+//       >
+//         <OutlineDeleteModal
+//           handleClose={deleteCloseFun}
+//           selectedItem={selectedTemplate}
+//           updateFun={dataUpdate}
+//           setIsSucces={setIsSucces}
+//           setMessage={setMessage}
+//           setOpenSnackbar={setOpenSnackbar}
+//         />
+//       </Dialog>
+//     </>
+//   );
+// }
+// Home.propTypes = {};
+
+// export default Home;
